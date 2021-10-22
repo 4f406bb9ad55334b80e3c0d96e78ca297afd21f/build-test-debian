5f03591d935b24aa1c4369a7a507bbb87d433f23
paledega <38860643+paledega@users.noreply.github.com>
2021-10-22 15:28:35 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
-echo "Hello world" > /output/hello.txt
-# This script run in debian chroot
-# Artifact files must move in /output directory
+apt install make gcc mtools xorriso squashfs-tools erofs-utils
+git clone https://gitlab.com/tearch-linux/applications-and-tools/teaiso
+cd teaiso
+make && make install
+cd /build
+mkteaiso -c debian
+mkteaiso -p debian

f75ef8614c57a999fe31649ed921f40e0f81167b
paledega <38860643+paledega@users.noreply.github.com>
2021-10-23 12:31:32 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt install xorriso grub-pc-bin grub-efi mtools make python3 \
     dosfstools e2fsprogs squashfs-tools python3-yaml \
-    gcc wget curl unzip xz-utils debootstrap -y
+    gcc wget curl unzip xz-utils debootstrap git -y
 git clone https://gitlab.com/tearch-linux/applications-and-tools/teaiso
 cd teaiso
 make && make install

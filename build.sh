ad1295d61d85b47d99544dac33325e143b10c854
paledega <38860643+paledega@users.noreply.github.com>
2021-10-27 18:13:36 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 apt install xorriso grub-pc-bin grub-efi mtools make python3 \
     dosfstools e2fsprogs squashfs-tools python3-yaml \
     gcc wget curl unzip xz-utils debootstrap git -y
-git clone https://gitlab.com/tearch-linux/applications-and-tools/archstrap
-cd archstrap
-make install
-archstrap archlinux
+git clone https://gitlab.com/tearch-linux/applications-and-tools/teaiso
+cd teaiso
+make && make install
+cd ..
+mkteaiso -c archlinux
+mkteaiso -p archlinux
